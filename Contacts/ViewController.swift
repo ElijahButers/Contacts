1e9ab0b7262d38a1c3082ce9224244747263c018
ElijahButers <Elijahbuters@gmail.com>
2017-11-22 22:26:26 +0200

    Add code to viewDidLoad() method to ViewController class that creates a navigation’s title and large title for navigation bar.

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.title = "Contacts"
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
 
 

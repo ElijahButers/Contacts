4fa0d9ca1e9b429636fa9d20e8cdfd55bbc579ca
ElijahButers <Elijahbuters@gmail.com>
2017-11-23 14:54:52 +0200

    Add a tableView(numberOfRowsInSection) and tableView(cellForRowAt) methods to ViewController class that create table rows and cells on the view.

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    let cellId = "cellId123123"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +18,18 @@ class ViewController: UITableViewController {
         
         navigationItem.title = "Contacts"
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    // MARK: -  TableView
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.textLabel?.text = "Something"
+        return cell
     }
 
 

004cf5a1abea9a2786dfe1243f12ffd042ed6586
ElijahButers <Elijahbuters@gmail.com>
2017-12-05 17:35:59 +0200

    Add a new ContactCell class, a UITableViewCell subclass to the project.

---
+++ Contacts/ContactCell.swift
@@ -0,0 +1,13 @@
+//
+//  ContactCell.swift
+//  Contacts
+//
+//  Created by User on 12/5/17.
+//  Copyright © 2017 User. All rights reserved.
+//
+
+import UIKit
+
+class ContactCell: UITableViewCell {
+    
+}

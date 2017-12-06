907642f3e4b535ef4b128c841ab2264724129b0e
ElijahButers <Elijahbuters@gmail.com>
2017-12-06 10:02:10 +0200

    Add code to ContactCell class that required in order to work with a cell.

--- Contacts/ContactCell.swift
+++ Contacts/ContactCell.swift
@@ -10,4 +10,11 @@ import UIKit
 
 class ContactCell: UITableViewCell {
     
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }

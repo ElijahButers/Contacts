0f1a5f8985ef36f10af2fbf839673d988a565db6
ElijahButers <Elijahbuters@gmail.com>
2017-12-11 15:33:41 +0200

    Update the two dimensional array of names with map function in ViewController class.

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -12,8 +12,8 @@ class ViewController: UITableViewController {
     
     let cellId = "cellId123123"
     var twoDimensionalArray = [
-            // ExpandableNames(isExpanded: true, names: ["Amy", "Bill", "Zack", "Steve", "Jack"]),
-            // ExpandableNames(isExpanded: true, names: ["Carl", "Chris", "Christina", "Cameron"]),
+        ExpandableNames(isExpanded: true, names: ["Amy", "Bill", "Zack", "Steve", "Jack"].map{ Contact(name: $0, hasFavorited: false) }),
+        ExpandableNames(isExpanded: true, names: ["Carl", "Chris", "Christina", "Cameron"].map{ Contact(name: $0, hasFavorited: false) }),
             ExpandableNames(isExpanded: true, names: [Contact(name: "Dan", hasFavorited: false)])]
     var showIndexPath = false
 

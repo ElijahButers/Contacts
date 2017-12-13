445184de6c6f5c2e42beba510ecee2e509c0d99d
ElijahButers <Elijahbuters@gmail.com>
2017-12-13 17:50:54 +0200

    Update the struct declaration in ExpandableNames class.

--- Contacts/ExpandableNames.swift
+++ Contacts/ExpandableNames.swift
@@ -12,10 +12,10 @@ import Contacts
 struct ExpandableNames {
     
     var isExpanded: Bool
-    var names: [Contact]
+    var names: [FavoritableContact]
 }
 
-struct Contact {
+struct FavoritableContact {
     
     let contact: CNContact
     var hasFavorited: Bool
